255024e58a74994303093f46876f10c527c0d3fa
Henrique (Jeovs) <heryque08@gmail.com>
2023-05-15 12:16:14 -0500

    aula 3

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,15 @@
+@   Table(name = "medicos")
+@Entity(name = "Medico")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
